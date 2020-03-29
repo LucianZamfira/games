3683712f4419efb2ac59ab6f6438bbb50f151a66
Lucian Zamfira <zamfira.lucian@gmail.com>
2020-03-29 02:56:10 +0200

    refactor: update layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function simulateClick(element) {
-	element.querySelector('.link').click();
+	element.querySelector('.card--clickable-target').click();
 }
 
 $(document).ready(function() {
@@ -15,8 +15,23 @@ $(document).ready(function() {
 	// Article click end
 
 	// Init audio
-	var audio = document.querySelector('audio');
-	audio.volume = 0.6;
+	var audioElement = document.querySelector('audio');
+	if (audioElement !== null) {
+		audioElement.volume = 0.2;
+		// audioElement.pause();
+	}
+	// audio.muted = true;
+	// audio.muted = !document.querySelector('audio').muted;
+
+	$('.mute-audio').on('click', function() {
+		event.preventDefault();
+		// audio.muted = true;
+		// audio.volume = 0.2;
+		// audio.muted = false;
+		$(this).toggleClass('muted');
+		audioElement.muted = !document.querySelector('audio').muted;
+		// audioElement.play();
+	});
 
 	$('.figure__spot').on('click', function() {
 		var spot = '.' + $(this).data('spot');
