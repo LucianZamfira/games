9bec90fb24cb163a5595d90c0810ab5ec8c88025
Lucian Zamfira <zamfira.lucian@gmail.com>
2020-03-29 19:14:46 +0300

    chore: change winning screen

--- js/main.js
+++ js/main.js
@@ -38,9 +38,7 @@ $(document).ready(function() {
 		audioSpark.play();
 
 		if ($('.figure__spot.show').length / 2 === 10) {
-			$('.congratulations').toggleClass('show');
-			// console.log('Felicitari, ai terminat jocul!');
-			// alert('Felicitari, ai terminat jocul!');
+			$('body').addClass('youwon');
 		}
 	});
 
